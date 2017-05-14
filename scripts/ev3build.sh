f78553a8731b1d00773f6be6ea0a4d57cf501b03
GearedTowardsProgress <geared@towards.progress>
2017-05-14 11:42:17 +0200

    updated

--- scripts/ev3build.sh
+++ scripts/ev3build.sh
@@ -1,6 +1,6 @@
 
 #!/bin/bash
-echo "=== EV3 Build Script v.1.1 by Xer0 ==="
+echo "=== EV3 Build Script v.1.2 by Xer0, peerfunk ==="
 #BUILDING CURRENT DIRECTORY
 FILES=`find . -type f -name "*.cpp" | tr -d ' '`
 #FILES="$FILES `find . -type f -name "*.h" | tr -d ' '`"
@@ -8,7 +8,7 @@ FILES=`find . -type f -name "*.cpp" | tr -d ' '`
 printf "Linking and Compiling files:\n"
 echo $FILES
 #sleep 3
-arm-linux-gnueabi-g++-5 -o EV3Scripts $FILES -lev3dev -L./ -std=c++14 -static -fpermissive
+arm-linux-gnueabi-g++ -o EV3Scripts $FILES -lev3dev -L./ -std=c++14 -static -fpermissive
 	echo "=== Building Done ==="
 if [ $# -eq 1 ]
 then
