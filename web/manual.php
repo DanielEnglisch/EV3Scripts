c4379d76da5a3fa5e9de7502f950adda9f26b51d
GearedTowardsProgress <geared@towards.progress>
2017-05-27 11:51:41 +0200

    added manual test

---
+++ web/manual.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Geared Towards Progress - Anleitung</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/manual.css" rel="stylesheet">
+
+  </head>
+  <body>
+  <div class="container">
+  <h2>Anleitung</h2>  
+  <div id="myCarousel" class="carousel slide" ata-interval="false">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="img/1.png" alt="New york" style="width:100%;">
+        <div class="info">
+          Nehmen sie ganz viel weed oida.
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="img/1.png" alt="New york" style="width:100%;">
+        <div class="info">
+          Danach stecken sie sich bananen in die Nase und pusten kräftig ihre Ohren durch
+          damit sie besser richen können sie sack.
+        </div>
+      </div>
+    
+      <div class="item">
+        <img src="img/1.png" alt="New york" style="width:100%;">
+        <div class="info">
+          Des is a guads zeig hob i gsogt herst!
+        </div>
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+   
+   </script>
+
+  </body>
+</html>
