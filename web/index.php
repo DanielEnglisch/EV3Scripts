37021e695a64346ba42c89a7ae33c5e3b41e2eec
GearedTowardsProgress <geared@towards.progress>
2017-05-27 11:23:14 +0200

    updated design

--- web/index.php
+++ web/index.php
@@ -1,28 +1,51 @@
+<?php
+  function spawnBrick($r,$g,$b){
+    echo '
+      <div class="brick">
+        <div class="body" style="background-color: rgb('.$r.','.$g.','.$b.');">
+          <div class="left" style="background-color: rgb('.$r.','.$g.','.$b.');"></div>
+          <div class="right" style="background-color: rgb('.$r.','.$g.','.$b.');"></div>
+        </div>
+      </div>';
+  }
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="de">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Bootstrap 101 Template</title>
-
-    <!-- Bootstrap -->
+    <title>Geared Towards Progress - Controller</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/brick.css" rel="stylesheet">
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
   </head>
   <body>
-    <h1>Hello, world!</h1>
+  <div class="container">
+      <h1>Momentanes Rezept</h1>
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+      <div class="container" id="brick-container">
+        <?php
+        $file = fopen("in.txt", "r");
+        if ($file) {
+          while (($line = fgets($file)) !== false) {
+              $s = explode(";", $line);
+              spawnBrick($s[0],$s[1],$s[2]);
+            }
+          fclose($file);
+        }
+      ?>
+
+      </div>
+      
+
+  </div>
+
     <script src="js/jquery-3.2.1.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
+
+   
+   </script>
+
   </body>
 </html>
