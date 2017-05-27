700f1399da47157110864263e8cf199aac0f2d46
GearedTowardsProgress <geared@towards.progress>
2017-05-27 10:15:11 +0200

    fixed css

--- web/index.php
+++ web/index.php
@@ -5,7 +5,7 @@
 	        <div class ="brick_bottom" style="background-color:rgb('.$r.','.$g.','.$b.');"></div>
 	        <div class ="brick_side" style="background-color:rgb('.$r.','.$g.','.$b.');"></div>
 	        <div class ="brick_head" style="background-color:rgb('.$r.','.$g.','.$b.');"></div>
-	    </div>';
+	        </div>';
   }
 
 ?>
@@ -20,8 +20,6 @@
       <?php
         $file = fopen("in.txt", "r");
         if ($file) {
-          $brick_count  = fgets($file);
-          echo  "<div class='numb'>$brick_count - Bricks gefunden!</div></br>";
           while (($line = fgets($file)) !== false) {
               $s = explode(";", $line);
               spawnBrick($s[0],$s[1],$s[2]);
@@ -30,12 +28,7 @@
         }
       ?>
 	</div>
-	 
-	<div id="control">
-		<h1> Steuerung </h1>
-	    <button type="button" style="left:20%;" >STEINE EINLESEN!</button>
-	    <button type="button"  style="right:20%;">STEINE EINSAMMELN!</button> 
-	</div>
+	
 </body>
 </html>
 
