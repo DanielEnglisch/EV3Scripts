c7d5b6f1eb3b83ea399cedb833c7a93c04907aa1
GearedTowardsProgress <geared@towards.progress>
2017-05-28 16:56:58 +0200

    removed

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,11 @@
 #include <iostream>
-#include "LineFollow.h"
 using namespace std;
 int main(){
    	
-   	// cout << "0;0;0" << endl;
-    //    for(int r = 0; r<255; ++r){
-    //    for(int g = 0; g<255; ++g){
-    //    for(int b = 0; b<255; ++b){
-    //        cout << r << ';'<< g << ';'<< b << endl;
-    //        }
-    //        }
-    //        }
-       
-    robot r;
-    
-    r.read_recepie();
-    //r.get_stones;
-
-   	cout << "0;0;0" << endl;
+   cout << "0;0;0" << endl;
+   for(int r = 0; r<255; r++)
+   	cout << r << ';'<< r << ';'<< r << endl;
+   cout << "0;0;0" << endl;
 
     return 0;
 }
