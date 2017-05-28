7379f511bc09fd139948bec615a355fbcc2e80e8
GearedTowardsProgress <geared@towards.progress>
2017-05-28 17:43:31 +0200

    notfix

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,9 @@
 #include <iostream>
-#include "LineFollow.h"
 using namespace std;
 int main(){
    	
-   cout << "0;0;0" << endl;
-   for(int r = 0; r<255; r+=25)
-   	cout << r << ';'<< r << ';'<< r << endl;
-   	cout << "0;0;0" << endl;
-
-
-   	robot r();
-   	r.test();
-
+   cout << "Reading recipe..." << endl;
+   
     return 0;
 }
 
--- web/index.php
+++ web/index.php
@@ -2,6 +2,24 @@
   function spawnBrick($r,$g,$b,$num){
     return '<div class="brick" id="brick'.$num.'"><div class="body" style="background-color: rgb('.$r.','.$g.','.$b.');"><div class="left" style="background-color: rgb('.$r.','.$g.','.$b.');"></div><div class="right" style="background-color: rgb('.$r.','.$g.','.$b.');"></div></div></div>';
   }
+
+  function getRecipe(){
+    while (@ ob_end_flush());
+            $num = 1;
+            $handle = fopen("in.txt", "r");
+            if ($handle) {
+                while (($line = fgets($handle)) !== false) {
+                   $a = explode(';', $line );
+                  $code = spawnBrick($a[0],$a[1],$a[2],$num);
+                  $log =  $code . '<script>$("#brick'.$num.'").appendTo("#block-container")</script>';
+                  echo $log;
+                @ flush();
+                $num++;
+                }
+
+                fclose($handle);
+            }
+  }
 ?>
 <!DOCTYPE html>
 <html lang="de">
@@ -12,20 +30,16 @@
     <title>Geared Towards Progress - Controller</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/brick.css" rel="stylesheet">
-
   </head>
   <body>
   <div class="container" id="content">
       <h1>Robo Webinterface</h1>
-      <div class="container" id="recipe">
-      <h2>Rezept</h2>
-        <div class="container" id="block-container">
-        </div>
-      </div>
+      
       <div class="container" id="actions">
         <h2>Aktionen</h2>
         <div class="btn-group" role="group" aria-label="...">
           <a href="?act=readRecipe" class="btn btn-info" role="button">ReadRecipe</a>
+          <a href="?act=getRecipe" class="btn btn-info" role="button">GetRecipe</a>
         </div>
       </div>
       <script src="js/jquery-3.2.1.min.js"></script>
@@ -33,32 +47,29 @@
       <div class="container" id="console">
         <?php
           if(isset($_GET['act'])){
-            echo "<h2>Konsole</h2>";
-            $num = 1;
-            while (@ ob_end_flush());
+            
+            if($_GET['act'] == 'getRecipe'){
+              echo'<div class="container" id="recipe">
+      <h2>Rezept</h2>
+        <div class="container" id="block-container">
+        </div>
+      </div>';
+              getRecipe();
+            }else if ($_GET['act'] == 'readRecipe'){
 
-            $proc = popen('./' . $_GET['act'], 'r');
-            $code = '';
-            echo '<pre>';
-            while (!feof($proc))
-            {
-                $log = fread($proc, 4096);
-                if($_GET['act'] == 'readRecipe'){
-                  $a = explode(';', $log);
-                  $code = spawnBrick($a[0],$a[1],$a[2],$num);
-                  $log = $log . $code . '<script>$("#brick'.$num.'").appendTo("#block-container")</script>';
-                }
+              echo "<h2>Konsole</h2>";
+              while (@ ob_end_flush());
 
-                echo $log;
-                @ flush();
-                $num++;
-            }
-            echo '</pre>';
-            if(isset($_GET['act'])){
-              echo '<script>$("#brick'.($num - 1) .'").remove();</script>';
-            }
-             pclose($proc);
+              $proc = popen('./readRecipe', 'r');
+              echo '<pre>';
+              while (!feof($proc))
+              {
+                  echo fread($proc, 4096);
+                  @ flush();
+              }
+              echo '</pre>';
           }
+        }
 
         ?>
       </div>
