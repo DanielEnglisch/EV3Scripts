0c1c17b8234205e457df870c2cdc49115c0c91f0
GearedTowardsProgress <geared@towards.progress>
2017-05-28 14:33:42 +0200

    gg

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
+#include <iostream>
 #include "LineFollow.h"
-  
+using namespace std;
 int main(){
-   
+   	
+   	cout << "0;0;0" << endl;
+
     robot r;
     r.read_recepie();
 
+   	cout << "0;0;0" << endl;
+
+    return 0;
 }
 
