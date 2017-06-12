713b39c45bc4ccc0ad92f2fdfb730f5d41a2c8ef
peerfunk <peerfunk@gmail.com>
2017-06-12 10:17:47 +0200

    d

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,13 @@ int main(){
 // motor rm(OUTPUT_A);
 // motor lm(OUTPUT_D);
 robot r;
-r.test();
+r.read_recipe();
+r.wait();
+r.wait();
+r.wait();
 // Claw t;
+r.wait();
+r.get_stones();
 // t.open();
 // t.lift();
 // t.close();
